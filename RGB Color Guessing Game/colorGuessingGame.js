f2c4b1dd3789b7eb5f15a492869ff29491e0cab2
iMusashi <sharma.keshav39@gmail.com>
2018-04-08 23:51:55 +0530

    Verify correctly clicked div
    We are verifying if correct div has been clicked, if not, we change the div color to body color.

--- RGB Color Guessing Game/colorGuessingGame.js
+++ RGB Color Guessing Game/colorGuessingGame.js
@@ -1,24 +1,30 @@
 colors=[
-    "RGB(255, 0, 0)",
-    "RGB(255, 255, 0)",
-    "RGB(255, 0, 255)",
-    "RGB(0, 255, 255)",
-    "RGB(0, 255, 0)",
-    "RGB(0, 0, 255)"
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)"
 ]   
 
 var lstColorBlocks = document.querySelectorAll(".mixedColorBlock");
 var bodyObj = document.querySelector("body");
+var pickedColor = colors[3];
+var rgbHeaderDisplayObj = document.querySelector("#rgbHeaderValue");
+rgbHeaderDisplayObj.textContent = String(pickedColor).toUpperCase();
+
 for(var i = 0; i < lstColorBlocks.length; i++)
 {
     lstColorBlocks[i].style.backgroundColor = colors[i];
     lstColorBlocks[i].addEventListener("click", function(){
         if(this.style.backgroundColor != pickedColor)
-        { this.style.backgroundColor == bodyObj.style.backgroundColor; }
-        else{}
+        { 
+            this.style.backgroundColor = bodyObj.style.backgroundColor;
+        }
+        else
+        {
+            
+        }
     });
 }
 
-var pickedColor = colors[3];
-var rgbHeaderDisplayObj = document.querySelector("#rgbHeaderValue");
-rgbHeaderDisplayObj.textContent = pickedColor;
