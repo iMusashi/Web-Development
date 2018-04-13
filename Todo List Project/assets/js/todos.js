8638f9aa4fcdb0290279a5782a53f859e6397e75
iMusashi <sharma.keshav39@gmail.com>
2018-04-13 23:58:21 +0530

    Styling the TodoList
    Made changes in all three files to incorporate better styling and execution.

--- Todo List Project/assets/js/todos.js
+++ Todo List Project/assets/js/todos.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-    var newItem = $("#newTodoItem");
+    var newItem = $("input");
     var addItem = $("#idOpenInput");
-    
+    var mainList = $("#itemsList");
+    var ulItem = $("ul");
+    var liItem =$("li");
     addItem.on("click",function(){
         $(this).closest("div").find("input").toggleClass("dispnone");
     });
@@ -9,6 +11,24 @@ $(document).ready(function(){
         if(event.which != 13)
             return;
         var todoText = $(this).val();
-        var todoElement = $("<div>" + todoText + "</div>")                
-    })
+        if(todoText == "")
+            return;
+        $(this).val("");
+        var todoElement = $("<li><span class='fas fa-trash-alt'></span> " + todoText + "</li>");
+        mainList.append(todoElement);                
+    });
+    
+    ulItem.on("click","li",function(){
+        $(this).toggleClass("completedItem");
+    });
+
+    var deleteIcon = $(".fa-trash-alt");
+    ulItem.on("click",".fa-trash-alt",function(event){
+        $(this).closest("li").fadeOut(function(){$(this).remove();});
+        event.stopPropagation();
+    }); 
+
+    liItem.on("mouseenter",function(){
+        $(this).find("span").toggleClass("fas fa-trash-alt");
+    });
 });
